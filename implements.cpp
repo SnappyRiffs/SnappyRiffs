984e71b0b685f8cf7bf7489d4935858730e4f2a2
SnappyRiffs <97476091+SnappyRiffs@users.noreply.github.com>
2024-11-01 21:11:42 -0700

    Update and rename implements.cpp to implements.h

--- implements.cpp
+++
@@ -1,9 +0,0 @@
-void def(mode, int var[num]){
-    int len = sizeof(myNumbers) / sizeof(myNumbers[0]);
-    for(int i = 0;i<len;i++){
-        def(mode,var[i]);
-    }
-}
-void def(mode,var)(
-    pinMode(var,mode);
-)
---
+++ implements.h
@@ -0,0 +1,7 @@
+#ifndef implements
+#define implements
+class implements{
+    public:
+        void def(mode,var);
+        void def(mode,int var[num]);
+}
