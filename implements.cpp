88d9d75647c14223e58a5e4b590f5e1e30e1508a
SnappyRiffs <97476091+SnappyRiffs@users.noreply.github.com>
2024-11-01 21:12:29 -0700

    Create implements.cpp

---
+++ implements.cpp
@@ -0,0 +1,9 @@
+void implements::def(mode, int var[num]){
+    int len = sizeof(myNumbers) / sizeof(myNumbers[0]);
+    for(int i = 0;i<len;i++){
+        def(mode,var[i]);
+    }
+}
+void implements::def(mode,var)(
+    pinMode(var,mode);
+)
